41b61cb761e9ab02b8aeb0dd2f5ed106d54603a0
kpcyrd <git@rxv.cc>
2023-12-10 20:21:03 +0100

    Cleanup code

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
+# rp2040-37c3-oled
+
+```
+rustup target add thumbv6m-none-eabi
+cargo build --release
+elf2uf2-rs -d
+```
+
+## Regenerate frames
 
 ```
 convert frame1.png -monochrome -negate frame1.pbm
 # use a hex editor, search for start of null bytes
 tail -c +11 frame1.pbm > frame1.raw
 ```
-
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
-//! Rainbow effect color wheel using the onboard NeoPixel on an Waveshare RP2040 Zero board
-//!
-//! This flows smoothly through various colors on the onboard NeoPixel.
-//! Uses the `ws2812_pio` driver to control the NeoPixel, which in turns uses the
-//! RP2040's PIO block.
 #![no_std]
 #![no_main]
 
-use core::iter::once;
 use defmt_rtt as _;
 use embedded_graphics::{
     image::{Image, ImageRaw},
@@ -17,12 +11,10 @@ use embedded_hal::timer::CountDown;
 use fugit::ExtU32;
 use fugit::RateExtU32;
 use panic_halt as _;
-use smart_leds::{brightness, SmartLedsWrite, RGB8};
 use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
 use usb_device::class_prelude::UsbBusAllocator;
 use usb_device::device::{UsbDeviceBuilder, UsbVidPid};
 use usbd_serial::SerialPort;
-use usbd_serial::UsbError;
 use usbd_serial::USB_CLASS_CDC;
 use waveshare_rp2040_zero::entry;
 use waveshare_rp2040_zero::{
@@ -30,7 +22,6 @@ use waveshare_rp2040_zero::{
         clocks::{init_clocks_and_plls, Clock},
         i2c::I2C,
         pac,
-        pio::PIOExt,
         timer::Timer,
         usb::UsbBus,
         watchdog::Watchdog,
@@ -38,7 +29,6 @@ use waveshare_rp2040_zero::{
     },
     Pins, XOSC_CRYSTAL_FREQ,
 };
-use ws2812_pio::Ws2812;
 
 const FRAMES: &[ImageRaw<BinaryColor>] = &[
     ImageRaw::new(include_bytes!("../video/frame1.raw"), 128),
@@ -196,6 +186,8 @@ const FRAMES: &[ImageRaw<BinaryColor>] = &[
 #[entry]
 fn main() -> ! {
     let mut pac = pac::Peripherals::take().unwrap();
+
+    // Configure clocks and timers
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let clocks = init_clocks_and_plls(
         XOSC_CRYSTAL_FREQ,
@@ -209,6 +201,10 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
+    let timer = Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
+    let mut delay = timer.count_down();
+
+    // Configure gpio
     let sio = Sio::new(pac.SIO);
     let pins = Pins::new(
         pac.IO_BANK0,
@@ -216,9 +212,6 @@ fn main() -> ! {
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
-
-    let timer = Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
-    let mut delay = timer.count_down();
 
     // Configure display
     let i2c = I2C::i2c1(
@@ -233,25 +226,6 @@ fn main() -> ! {
     let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
         .into_buffered_graphics_mode();
     display.init().unwrap();
-
-    let raw: ImageRaw<BinaryColor> = ImageRaw::new(include_bytes!("../video/frame1.raw"), 128);
-
-    let im = Image::new(&raw, Point::new(0, 13));
-
-    im.draw(&mut display).unwrap();
-
-    display.flush().unwrap();
-
-    // Configure the addressable LED
-    let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
-    let mut ws = Ws2812::new(
-        // The onboard NeoPixel is attached to GPIO pin #16 on the Feather RP2040.
-        pins.neopixel.into_function(),
-        &mut pio,
-        sm0,
-        clocks.peripheral_clock.freq(),
-        timer.count_down(),
-    );
 
     // Configure USB serial
     let usb_bus = UsbBusAllocator::new(UsbBus::new(
@@ -268,68 +242,27 @@ fn main() -> ! {
         .build();
 
     // enter loop
-    let mut n = 0;
+    let mut iter = [].iter();
     loop {
-        let Some(raw) = FRAMES.get(n) else {
-            n = 0;
+        // get next frame or restart iterator
+        let Some(raw) = iter.next() else {
+            iter = FRAMES.iter();
             continue;
         };
 
+        // draw image
         let im = Image::new(raw, Point::new(0, 13));
         im.draw(&mut display).unwrap();
         display.flush().unwrap();
 
+        // sleep for frame rate
         delay.start(50.millis());
         let _ = nb::block!(delay.wait());
 
-        n = n + 1;
-
-        /*
-        // magic wheel
-        ws.write(brightness(once(wheel(n)), 32)).unwrap();
-        n = n.wrapping_add(1);
-
-        delay.start(50.millis());
-        let _ = nb::block!(delay.wait());
-
-        // skip if no usb device connected
-        if !usb_dev.poll(&mut [&mut serial]) {
-            continue;
+        // read and discard any serial data sent to us
+        if usb_dev.poll(&mut [&mut serial]) {
+            let mut buf = [0u8; 64];
+            serial.read(&mut buf[..]).ok();
         }
-
-        // read and discard any data sent to us
-        let mut buf = [0u8; 64];
-        serial.read(&mut buf[..]).ok();
-
-        // send some data
-        match serial.write(b"hello!\r\n") {
-            Ok(_count) => {
-                // count bytes were written
-            }
-            // no data could be written
-            Err(UsbError::WouldBlock) => (),
-            // an error occurred
-            Err(_err) => (),
-        };
-        */
-    }
-}
-
-/// Convert a number from `0..=255` to an RGB color triplet.
-///
-/// The colours are a transition from red, to green, to blue and back to red.
-fn wheel(mut wheel_pos: u8) -> RGB8 {
-    wheel_pos = 255 - wheel_pos;
-    if wheel_pos < 85 {
-        // No green in this sector - red and blue only
-        (255 - (wheel_pos * 3), 0, wheel_pos * 3).into()
-    } else if wheel_pos < 170 {
-        // No red in this sector - green and blue only
-        wheel_pos -= 85;
-        (0, wheel_pos * 3, 255 - (wheel_pos * 3)).into()
-    } else {
-        // No blue in this sector - red and green only
-        wheel_pos -= 170;
-        (wheel_pos * 3, 255 - (wheel_pos * 3), 0).into()
     }
 }
